7040afba125a90be136a4aa4484ffd23143042c7
Artem <arte.kats@gmail.com>
2025-08-20 21:17:46 +0300

    Решение срезами, можно оптимизировать по памяти и времени

---
+++ Med/Rotate_array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        return nums
+
+
+
+solution = Solution()
+print(solution.rotate([1,2],7))
